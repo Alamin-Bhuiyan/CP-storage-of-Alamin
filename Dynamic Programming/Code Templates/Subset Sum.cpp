5d3c3614be7cd75d87261d0af04a4b0ed08b15ea
MD AL AMIN BHUIYAN <amialaminb@gmail.com>
2021-09-21 22:04:42 +0600

    Subset sum/Coin change (Count)

---
+++ Dynamic Programming/Code Templates/Subset Sum.cpp
@@ -0,0 +1,35 @@
+Link: https://www.youtube.com/watch?v=_gPcYovP7wc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=7
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    long long n,i,j,k,sum;
+    cin>>n>>sum;
+    long long a[n+1];
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    long long dp[n+1][sum+1];
+    memset(dp,0,sizeof(dp));
+    for(i=0;i<=n;i++)dp[i][0]=1;
+    for(i=0;i<=sum;i++)dp[0][i]=1;
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=sum;j++)
+        {
+            if(a[i-1]<=j)
+                dp[i][j]=dp[i-1][j-a[i-1]]+dp[i-1][j];
+            else
+                dp[i][j]=dp[i-1][j];
+        }
+    }
+  //dp[n][sum] have the total number of subsets' sum which are equal to sum
+    if(dp[n][sum])
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+}
